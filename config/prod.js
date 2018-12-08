a3fcf1ac2fc5beb9ff847e896c550c1e4a333ac1
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-12-08 19:09:27 +0100

    put the base url in config

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: process.env.MONGO_URL,
-  jwtKey: process.env.JWT_KEY
+  jwtKey: process.env.JWT_KEY,
+  baseURL: process.env.BACE_URL
 };
--- routes/game.js
+++ routes/game.js
@@ -7,6 +7,7 @@ const fse = require("fs-extra");
 const router = express.Router();
 const User = require("../models/User");
 const Game = require("../models/Game");
+const keys = require("../config/keys");
 
 router.get("/:id", (req, res) => {
   Game.findOne({ user: req.params.id })
@@ -57,56 +58,60 @@ const upload = multer({
   fileFilter
 });
 
-router.post("/game-data/:id/:gamesize", upload.array("gameImgs"), (req, res) => {
-console.log(req.params);
-console.log(req.files);
+router.post(
+  "/game-data/:id/:gamesize",
+  upload.array("gameImgs"),
+  (req, res) => {
+    console.log(req.params);
+    console.log(req.files);
 
-  if (req.files && req.files.length == req.params.gamesize) {
-    Game.findOne({ user: req.params.id }).then(game => {
-      if (game) {
-        const imgsUrl = [];
-        req.files.map(Url => {
-          imgsUrl.push({
-            cardName: `${Url.filename}`,
-        //    img: `https://memory-game-7.herokuapp.com/uploads/game/${
-            img: `http://localhost:5000/uploads/game/${
-              req.params.id
-            }/${Url.filename}`
+    if (req.files && req.files.length == req.params.gamesize) {
+      Game.findOne({ user: req.params.id }).then(game => {
+        if (game) {
+          const imgsUrl = [];
+          req.files.map(Url => {
+            imgsUrl.push({
+              cardName: `${Url.filename}`,
+              //   img: `https://memory-game-7.herokuapp.com/uploads/game/${
+              img: `${keys.baseURL}/uploads/game/${req.params.id}/${
+                Url.filename
+              }`
+            });
           });
-        });
-        if (imgsUrl.length == req.params.gamesize) {
-          game.imgsGame = imgsUrl;
-          game.gamesize = req.params.gamesize;
-          game.save();
-        return  res.json(game);
-          console.log('data from DB change',game)
+          if (imgsUrl.length == req.params.gamesize) {
+            game.imgsGame = imgsUrl;
+            game.gamesize = req.params.gamesize;
+            game.save();
+            return res.json(game);
+            console.log("data from DB change", game);
+          }
+        } else {
+          const imgsUrl = [];
+          req.files.map(Url => {
+            // imgsUrl.push(`http://localhost:5000/uploads/game/${Url.filename}`)
+            imgsUrl.push({
+              cardName: `${Url.filename}`,
+              //  img: `https://memory-game-7.herokuapp.com/uploads/game/${
+              img: `${keys.baseURL}/uploads/game/${req.params.id}/${
+                Url.filename
+              }`
+            });
+          });
+          console.log("imgs url fixed", imgsUrl);
+          if (imgsUrl.length == req.params.gamesize) {
+            return new Game({
+              user: req.params.id,
+              gamesize: req.params.gamesize,
+              imgsGame: imgsUrl
+            }).save((err, data) => {
+              return res.json(data);
+              console.log("data from DB", data);
+            });
+          }
         }
-      } else {
-        const imgsUrl = [];
-        req.files.map(Url => {
-        // imgsUrl.push(`http://localhost:5000/uploads/game/${Url.filename}`)
-          imgsUrl.push({
-            cardName: `${Url.filename}`,
-          //  img: `https://memory-game-7.herokuapp.com/uploads/game/${
-             img: `http://localhost:5000/uploads/game/${req.params.id}/${Url.filename}`
-          });
-        });
-      console.log('imgs url fixed',imgsUrl);
-        if (imgsUrl.length == req.params.gamesize) {
-
-
-          return new Game({
-            user: req.params.id,
-            gamesize: req.params.gamesize,
-            imgsGame: imgsUrl
-          }).save((err, data) => {
-            return res.json(data);
-            console.log('data from DB',data)
-          });
-        }
-      }
-    });
+      });
+    }
   }
-});
+);
 
 module.exports = router;
